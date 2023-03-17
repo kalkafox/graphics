e8f1b25726b72769ade5d5d6bad98e5e05d1acc2
Brandon Boudreaux <hf3209@wayne.edu>
2023-03-16 21:19:59 -0400

    Added glad

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -5,5 +5,5 @@
 #include "Triangle.h"
 
 PhyG::Triangle::Triangle(std::string vertex_shader_location, std::string fragment_shader_location) : PhyG::RenderObject(vertex_shader_location, fragment_shader_location){
-    //glBufferData();
+    UnbindEBO();
 }
